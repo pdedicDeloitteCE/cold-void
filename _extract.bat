24d264b033b6ef244d2d3697fccc1bc6336d866f
Peter Dedic <peter.dedic@theglobalfund.org>
2017-03-02 11:17:36 +0100

    parametrized extract.bat fix

--- _extract.bat
+++ _extract.bat
@@ -11,11 +11,12 @@ IF "%JAVA_HOME%" == "" (
 )
 
 rem SET URL=sfdc.endpoint=%1
-rem SET URL=sfdc.endpoint=https://test.salesforce.com
+SET URL=sfdc.endpoint=https://test.salesforce.com
 SET USR=sfdc.username=%1
 rem SET USR=sfdc.username=peter.dedic@theglobalfund.org.2.aimdev1
 SET PWD=sfdc.password=%2
-SET PWD=sfdc.password=da36812bb56c4ebebe8aff6deca348b7d3e5843756fc97e2222512f28af664ec2ff31d586ac3c10f
+rem SET PWD=sfdc.password=da36812bb56c4ebebe8aff6deca348b7d3e5843756fc97e2222512f28af664ec2ff31d586ac3c10f
+
 
 "%JAVA_HOME%"\bin\java -cp "%DATALOADER_HOME%"\dataloader-36.0.0-uber.jar -Dsalesforce.config.dir=conf com.salesforce.dataloader.process.ProcessRunner %URL% %USR% %PWD% process.name=metaExtract | find /V "INFO"
 "%JAVA_HOME%"\bin\java -cp "%DATALOADER_HOME%"\dataloader-36.0.0-uber.jar -Dsalesforce.config.dir=conf com.salesforce.dataloader.process.ProcessRunner %URL% %USR% %PWD% process.name=settingsExtract | find /V "INFO"
